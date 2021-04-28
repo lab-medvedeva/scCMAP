75596dfa089992d68e1333d8aeb374011536d388
Iuliia Vyshkvorkina <yuliya.vyshkvorkina@phystech.edu>
2021-04-28 20:49:24 +0300

    Update cm_intregnet.sh

--- cm_intregnet.sh
+++ cm_intregnet.sh
@@ -1,11 +1,11 @@
-./sc-intregnet.sh --sc-intregnet-path "$INTREGNET_DIR" --tfs-in-combo 1 --start-sample-name "${start-type}"
---start-exp "$CMAP_INPUT_DIR/${start-type}_aggregated_exp.tsv" --target-sample-name "${target_type}"
---target-exp-ranking "$CMAP_INPUT_DIR/${start-type}_to_${target_type}_TFs_in_DEGs_promoters.txt"
+INTREGNET_DIR="$1"
+CMAP_INPUT_DIR="$1/data/CM_exp_data"
 
+./sc-intregnet.sh --sc-intregnet-path "$1" --tfs-in-combo 1             --start-sample-name "$2"             --start-exp "$CMAP_INPUT_DIR/$4"             --target-sample-name "$3"             --target-exp-ranking "$CMAP_INPUT_DIR/$6"
 
+export TF_path1="$1/data/$2/tfs_predictions/$3_to_$2_FinalVals.tsv"
 
-./sc-intregnet.sh --sc-intregnet-path "$INTREGNET_DIR" --tfs-in-combo 1             --start-sample-name "${target_type}"             --start-exp "$CMAP_INPUT_DIR/${target_type}_agg_exp.tsv"             --target-sample-name "${start-type}"             --target-exp-ranking "$CMAP_INPUT_DIR/Panc_to_${start-type}_TFs_in_DEGs_promoters.txt"
+./sc-intregnet.sh --sc-intregnet-path "$1" --tfs-in-combo 1             --start-sample-name "$3"             --start-exp "$CMAP_INPUT_DIR/$5"             --target-sample-name "$2"             --target-exp-ranking "$CMAP_INPUT_DIR/$7"
 
-
-export TF_path="${TFS_PRED_DIR}/${initial_celltype}_to_${SAMPLE_NAME}_FinalVals.tsv"
-python3 $WORKDIR/scripts/main/cmap.py --TF_path "$TF_path" > $TFS_PRED_DIR/cmap.csv
+export TF_path2="$1/data/$3/tfs_predictions/$2_to_$3_FinalVals.tsv"
+python3 $1/scripts/main/cmap2.py --TF_path1 "$TF_path1" --TF_path2 "$TF_path2"> cmap3.csv
